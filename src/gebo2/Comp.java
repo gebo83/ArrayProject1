e32d5946f9550aba9a516948f87f41e1e92d439e
olenab <olenab@OLENABO.nice.com>
2016-07-14 12:21:18 +0300

    test2

--- src/gebo2/Comp.java
+++ src/gebo2/Comp.java
@@ -7,16 +7,17 @@ public class Comp {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		int[] myArray1;
-//		int k = 0;
+		int k = 0;
 		myArray1 = new int[10];
-		for (int i=1; i<10; i++){
-//			k = k+1;
-		myArray1[i]=i*2-1;
+		for (int i=0; i<10; i++){
+			k = k+1;
+		myArray1[i]=i+k;
 					}
 		for (int i=0; i<10; i++){
 System.out.println(myArray1[i]);}
 
 	}
 }
+
 
 
